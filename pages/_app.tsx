38dc6a36b51f3976e83686645fc5a1d6a50d23f9
Kevin Palowa <kevinpalowa@gmail.com>
2023-05-19 13:39:47 +0700

    chore: initial setup

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
